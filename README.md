598ab8127b01a6778502af93fc881fb24f89c4ad
Theobano <divinedotun@gmail.com>
2022-12-07 16:21:23 +0100

    initial release

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Description
+
+This project is my personal portfolio. It was created with React.js
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { NavBar } from "./components";
+import ErrorFallback from "./components/ErrorFallback";
+import Loader from "./components/Loader";
+import NotFound from "./routes/NotFound";
+
+const Home = React.lazy(() => import("./routes/home/Home"));
+const About = React.lazy(() => import("./routes/about/About"));
+const Contact = React.lazy(() => import("./routes/contact/Contact"));
+const Projects = React.lazy(() => import("./routes/projects/Projects"));
+const Footer = React.lazy(() => import("./components/Footer"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <NavBar />
+      {/* <div> */}
+      <section className="main-container">
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ErrorBoundary FallbackComponent={ErrorFallback}>
+                  <Home />
+                </ErrorBoundary>
+              }
+            />
+            <Route
+              path="/about"
+              element={
+                <ErrorBoundary FallbackComponent={ErrorFallback}>
+                  <About />
+                </ErrorBoundary>
+              }
+            />
+            <Route
+              path="/contact"
+              element={
+                <ErrorBoundary FallbackComponent={ErrorFallback}>
+                  <Contact />
+                </ErrorBoundary>
+              }
+            />
+            <Route
+              path="/projects"
+              element={
+                <ErrorBoundary FallbackComponent={ErrorFallback}>
+                  <Projects />
+                </ErrorBoundary>
+              }
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </section>
+      <Suspense>
+        <Footer />
+      </Suspense>
+      {/* </div> */}
     </div>
   );
 }
